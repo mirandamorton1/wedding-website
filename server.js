db096fd7e713b83879a95e13c0b37127be3e9686
mirandamorton1 <107001559+mirandamorton1@users.noreply.github.com>
2022-12-26 16:42:09 -0600

    express added

---
+++ server.js
@@ -0,0 +1,169 @@
+const express = require('express');
+const path = require('path');
+// Import and require mysql2
+// const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+// Express middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => 
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/questions', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/pages/questions.html'))
+);
+
+app.get('/photos', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/pages/photos.html'))
+);
+
+app.get('/party', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/pages/party.html'))
+);
+
+app.get('/registry', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/pages/registry.html'))
+);
+
+app.get('/rsvp', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/pages/rsvp.html'))
+);
+
+app.get('/shuttle', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/pages/shuttle.html'))
+);
+
+app.get('/story', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/pages/story.html'))
+);
+
+app.get('/todo', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/pages/todo.html'))
+);
+
+app.get('/travel', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/pages/travel.html'))
+);
+
+// Connect to database
+// const db = mysql.createConnection(
+//   {
+//     host: 'localhost',
+//     // MySQL username,
+//     user: 'root',
+//     // TODO: Add MySQL password here
+//     password: 'rootroot',
+//     database: 'movies_db'
+//   },
+//   console.log(`Connected to the movies_db database.`)
+// );
+
+// // Create a movie
+// app.post('/api/new-movie', ({ body }, res) => {
+//   const sql = `INSERT INTO movies (movie_name)
+//     VALUES (?)`;
+//   const params = [body.movie_name];
+  
+//   db.query(sql, params, (err, result) => {
+//     if (err) {
+//       res.status(400).json({ error: err.message });
+//       return;
+//     }
+//     res.json({
+//       message: 'success',
+//       data: body
+//     });
+//   });
+// });
+
+// // Read all movies
+// app.get('/api/movies', (req, res) => {
+//   const sql = `SELECT id, movie_name AS title FROM movies`;
+  
+//   db.query(sql, (err, rows) => {
+//     if (err) {
+//       res.status(500).json({ error: err.message });
+//        return;
+//     }
+//     res.json({
+//       message: 'success',
+//       data: rows
+//     });
+//   });
+// });
+
+// // Delete a movie
+// app.delete('/api/movie/:id', (req, res) => {
+//   const sql = `DELETE FROM movies WHERE id = ?`;
+//   const params = [req.params.id];
+  
+//   db.query(sql, params, (err, result) => {
+//     if (err) {
+//       res.statusMessage(400).json({ error: res.message });
+//     } else if (!result.affectedRows) {
+//       res.json({
+//       message: 'Movie not found'
+//       });
+//     } else {
+//       res.json({
+//         message: 'deleted',
+//         changes: result.affectedRows,
+//         id: req.params.id
+//       });
+//     }
+//   });
+// });
+
+// // Read list of all reviews and associated movie name using LEFT JOIN
+// app.get('/api/movie-reviews', (req, res) => {
+//   const sql = `SELECT movies.movie_name AS movie, reviews.review FROM reviews LEFT JOIN movies ON reviews.movie_id = movies.id ORDER BY movies.movie_name;`;
+//   db.query(sql, (err, rows) => {
+//     if (err) {
+//       res.status(500).json({ error: err.message });
+//       return;
+//     }
+//     res.json({
+//       message: 'success',
+//       data: rows
+//     });
+//   });
+// });
+
+// // BONUS: Update review name
+// app.put('/api/review/:id', (req, res) => {
+//   const sql = `UPDATE reviews SET review = ? WHERE id = ?`;
+//   const params = [req.body.review, req.params.id];
+
+//   db.query(sql, params, (err, result) => {
+//     if (err) {
+//       res.status(400).json({ error: err.message });
+//     } else if (!result.affectedRows) {
+//       res.json({
+//         message: 'Movie not found'
+//       });
+//     } else {
+//       res.json({
+//         message: 'success',
+//         data: req.body,
+//         changes: result.affectedRows
+//       });
+//     }
+//   });
+// });
+
+// // Default response for any other request (Not Found)
+// app.use((req, res) => {
+//   res.status(404).end();
+// });
+
+app.listen(PORT, () => {
+  console.log(`Server running on port http://localhost:${PORT}`);
+});
