c372488a089d875149540394e34824a96744f1e0
mirandamorton1 <107001559+mirandamorton1@users.noreply.github.com>
2023-02-09 18:09:20 -0600

    formatted hero, bridal party, added footer

--- public/assets/js/rsvp.js
+++ public/assets/js/rsvp.js
@@ -64,62 +64,11 @@ updateBtn.onclick = function() {
         }
     })
 }
-
-///////////OPTIONAL HERE////////
-const addBtn = document.querySelector('#add-name-btn');
-
-addBtn.onclick = function () {
-    const nameInput = document.querySelector('#name-input');
-    const name = nameInput.value;
-    nameInput.value = "";
-
-    fetch('http://localhost:5000/insert', {
-        headers: {
-            'Content-type': 'application/json'
-        },
-        method: 'POST',
-        body: JSON.stringify({ name : name})
-    })
-    .then(response => response.json())
-    .then(data => insertRowIntoTable(data['data']));
-}
-
-function insertRowIntoTable(data) {
-    console.log(data);
-    const table = document.querySelector('table tbody');
-    const isTableData = table.querySelector('.no-data');
-
-    let tableHtml = "<tr>";
-
-    for (var key in data) {
-        if (data.hasOwnProperty(key)) {
-            if (key === 'dateAdded') {
-                data[key] = new Date(data[key]).toLocaleString();
-            }
-            tableHtml += `<td>${data[key]}</td>`;
-        }
-    }
-
-    tableHtml += `<td><button class="delete-row-btn" data-id=${data.id}>Delete</td>`;
-    tableHtml += `<td><button class="edit-row-btn" data-id=${data.id}>Edit</td>`;
-
-    tableHtml += "</tr>";
-
-    if (isTableData) {
-        table.innerHTML = tableHtml;
-    } else {
-        const newRow = table.insertRow();
-        newRow.innerHTML = tableHtml;
-    }
-}
-////////////OPTIONAL ENDS////////////
-
 function loadHTMLTable(data) {
     const table = document.querySelector('table tbody');
 
     if (data.length === 0) {
-        table.innerHTML = "<tr><td class='no-data' colspan='5'>No Data</td></tr>";
-        console.log(length);
+        table.innerHTML = "<tr><td class='no-data' colspan='5'>Sorry, we couldn't find a name that matched. Try entering it a different way!</td></tr>";
         return;
     }
 
